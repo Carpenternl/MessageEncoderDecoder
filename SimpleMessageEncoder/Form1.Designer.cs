87b30131d16a8cd7c18f059e255bb75939712bc1
Carpenternl <Hugo.Timmerman@outlook.com>
2018-04-19 09:49:20 +0200

    Load Text files functionality added.

--- SimpleMessageEncoder/Form1.Designer.cs
+++ SimpleMessageEncoder/Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.decodeFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.encodeFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(22, 22);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.decodeFileToolStripMenuItem,
+            this.encodeFileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(10, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1300, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // decodeFileToolStripMenuItem
+            // 
+            this.decodeFileToolStripMenuItem.Name = "decodeFileToolStripMenuItem";
+            this.decodeFileToolStripMenuItem.Size = new System.Drawing.Size(110, 27);
+            this.decodeFileToolStripMenuItem.Text = "Decode File";
+            this.decodeFileToolStripMenuItem.Click += new System.EventHandler(this.decodeFileToolStripMenuItem_Click);
+            // 
+            // encodeFileToolStripMenuItem
+            // 
+            this.encodeFileToolStripMenuItem.Name = "encodeFileToolStripMenuItem";
+            this.encodeFileToolStripMenuItem.Size = new System.Drawing.Size(108, 27);
+            this.encodeFileToolStripMenuItem.Text = "Encode File";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 33);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1300, 698);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1300, 731);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.8209F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem decodeFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem encodeFileToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- SimpleMessageEncoder/Form1.cs
+++ SimpleMessageEncoder/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,17 @@ namespace SimpleMessageEncoder
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void decodeFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "textfiles (*.txt) |"; 
+            openFileDialog1.ShowDialog();
+            string[] Filetext = File.ReadAllLines(openFileDialog1.FileName);
+            for (int i = 0; i < Filetext.Length; i++)
+            {
+                textBox1.Text += Filetext[i];
+            }
         }
     }
 }
